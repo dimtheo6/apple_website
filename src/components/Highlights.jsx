b62dd8f96197d5d3461d9164fad0c7dbfe8ce4fe
Dimitris <155730305+dimtheo6@users.noreply.github.com>
2025-07-25 11:19:38 +1000

    initial commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,18 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
+function Highlights() {
+  useGSAP(()=>{
+    gsap.to('#title',{opacity:1 , y:0})
+  },[])
+
+  return (
+    <section id='highlights' className='w-screen overflow-hidden h-full common-padding bg-zinc'>
+      <div className='screen-max-width'>
+        <h1 id='title' className='section-heading'>Get the highlights.</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Highlights
